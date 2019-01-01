67c32627091ce98860ad6096b075ac03b8425614
WhackerH <37672465+WhackerH@users.noreply.github.com>
2019-01-01 20:23:39 +0200

    Add files via upload

--- fbitools.lua
+++ fbitools.lua
@@ -1264,6 +1264,45 @@ function oopkey()
         end
     end
 end
+local russian_characters = {
+    [168] = '¨', [184] = '¸', [192] = 'À', [193] = 'Á', [194] = 'Â', [195] = 'Ã', [196] = 'Ä', [197] = 'Å', [198] = 'Æ', [199] = 'Ç', [200] = 'È', [201] = 'É', [202] = 'Ê', [203] = 'Ë', [204] = 'Ì', [205] = 'Í', [206] = 'Î', [207] = 'Ï', [208] = 'Ğ', [209] = 'Ñ', [210] = 'Ò', [211] = 'Ó', [212] = 'Ô', [213] = 'Õ', [214] = 'Ö', [215] = '×', [216] = 'Ø', [217] = 'Ù', [218] = 'Ú', [219] = 'Û', [220] = 'Ü', [221] = 'İ', [222] = 'Ş', [223] = 'ß', [224] = 'à', [225] = 'á', [226] = 'â', [227] = 'ã', [228] = 'ä', [229] = 'å', [230] = 'æ', [231] = 'ç', [232] = 'è', [233] = 'é', [234] = 'ê', [235] = 'ë', [236] = 'ì', [237] = 'í', [238] = 'î', [239] = 'ï', [240] = 'ğ', [241] = 'ñ', [242] = 'ò', [243] = 'ó', [244] = 'ô', [245] = 'õ', [246] = 'ö', [247] = '÷', [248] = 'ø', [249] = 'ù', [250] = 'ú', [251] = 'û', [252] = 'ü', [253] = 'ı', [254] = 'ş', [255] = 'ÿ',
+}
+function string.rlower(s)
+    s = s:lower()
+    local strlen = s:len()
+    if strlen == 0 then return s end
+    s = s:lower()
+    local output = ''
+    for i = 1, strlen do
+        local ch = s:byte(i)
+        if ch >= 192 and ch <= 223 then -- upper russian characters
+            output = output .. russian_characters[ch + 32]
+        elseif ch == 168 then -- ¨
+            output = output .. russian_characters[184]
+        else
+            output = output .. string.char(ch)
+        end
+    end
+    return output
+end
+function string.rupper(s)
+    s = s:upper()
+    local strlen = s:len()
+    if strlen == 0 then return s end
+    s = s:upper()
+    local output = ''
+    for i = 1, strlen do
+        local ch = s:byte(i)
+        if ch >= 224 and ch <= 255 then -- lower russian characters
+            output = output .. russian_characters[ch - 32]
+        elseif ch == 184 then -- ¸
+            output = output .. russian_characters[168]
+        else
+            output = output .. string.char(ch)
+        end
+    end
+    return output
+end
 function submenus_show(menu, caption, select_button, close_button, back_button)
     select_button, close_button, back_button = select_button or '»', close_button or 'x', back_button or '«'
     prev_menus = {}
@@ -4632,7 +4671,7 @@ end
 function fshp(pam)
     local f = io.open('moonloader\\fbitools\\shp.txt')
     for line in f:lines() do
-        if string.find(line, pam) then
+        if string.find(line, pam) or string.rlower(line):find(pam) or string.rupper(line):find(pam) then
             sampAddChatMessage(line, -1)
         end
     end
@@ -4641,7 +4680,7 @@ end
 function fyk(pam)
     local f = io.open('moonloader\\fbitools\\yk.txt')
     for line in f:lines() do
-      if string.find(line, pam) then
+      if string.find(line, pam) or string.rlower(line):find(pam) or string.rupper(line):find(pam) then
             sampAddChatMessage(line, -1)
       end
     end
@@ -4651,7 +4690,7 @@ end
 function ffp(pam)
     local f = io.open('moonloader\\fbitools\\fp.txt')
     for line in f:lines() do
-      if string.find(line, pam) then
+      if string.find(line, pam) or string.rlower(line):find(pam) or string.rupper(line):find(pam) then
             sampAddChatMessage(line, -1)
       end
     end
@@ -4661,7 +4700,7 @@ end
 function fak(pam)
     local f = io.open('moonloader\\fbitools\\ak.txt')
     for line in f:lines() do
-      if string.find(line, pam) then
+      if string.find(line, pam) or string.rlower(line):find(pam) or string.rupper(line):find(pam) then
             sampAddChatMessage(line, -1)
       end
     end
